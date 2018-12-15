3ef257762d90e43ba613247da1fbc762c4314835
chenby <87001700@qq.com>
2018-12-15 11:26:05 +0800

    改了模块引入（商品列表）

---
+++ src/lay/html/modules/goodlist.js
@@ -0,0 +1,82 @@
+layui.define(['table'],function(exports){
+  //商品列表渲染
+  
+  //得到teble对象
+  var table = layui.table;
+    
+    table.render({
+      elem: '#test'
+      ,url:'/goods'
+      ,toolbar: `<div class="layui-btn-container">
+      <button class="layui-btn layui-btn-sm" lay-event="getCheckData">获取选中行数据</button>
+      <button class="layui-btn layui-btn-sm" lay-event="getCheckLength">获取选中数目</button>
+      <button class="layui-btn layui-btn-sm" lay-event="isAll">验证是否全选</button>
+    </div>`
+      ,title: '商品列表'
+      ,cols: [[
+        {type: 'checkbox', fixed: 'left'}
+        ,{field:'SID', title:'ID', width:80, fixed: 'left', unresize: true, sort: true}
+        ,{field:'SNAME', title:'商品名称', width:120, edit: 'text'}
+        
+        ,{field:'STAG', title:'分类', width:80, edit: 'text', sort: true}
+        ,{field:'PRICE', title:'价格（原价）', width:120}
+        ,{field:'PRICE', title:'价格（现价）', width:120}
+        ,{field:'REPE', title:'库存', width:80, sort: true}
+        ,{field:'DIAN', title:'状态', width:120}
+        ,{field:'joinTime', title:'加入时间', width:150}
+        ,{fixed: 'right', title:'操作', toolbar: '#barDemo', width:200}
+      ]]
+      ,page: true
+      ,limit: 10
+    });
+    
+    //监听表格复选框选择
+    table.on('checkbox(demo)', function(obj){
+      console.log(obj)
+    });
+    //监听工具条
+    table.on('tool(barDemo)', function(obj){
+      var data = obj.data;
+      if(obj.event === 'detail'){
+        layer.msg('ID：'+ data.id + ' 的查看操作');
+      } else if(obj.event === 'del'){
+        layer.confirm('真的删除行么', function(index){
+          obj.del();
+          layer.close(index);
+        });
+      } else if(obj.event === 'edit'){
+        layer.alert('编辑行：<br>'+ JSON.stringify(data))
+      }
+    });
+    
+    var $ = layui.$, active = {
+      getCheckData: function(){ //获取选中数据
+        var checkStatus = table.checkStatus('idTest')
+        ,data = checkStatus.data;
+        layer.alert(JSON.stringify(data));
+      }
+      ,getCheckLength: function(){ //获取选中数目
+        var checkStatus = table.checkStatus('idTest')
+        ,data = checkStatus.data;
+        layer.msg('选中了：'+ data.length + ' 个');
+      }
+      ,isAll: function(){ //验证是否全选
+        var checkStatus = table.checkStatus('idTest');
+        layer.msg(checkStatus.isAll ? '全选': '未全选')
+      }
+    };
+    
+    $('.demoTable .layui-btn').on('click', function(){
+      var type = $(this).data('type');
+      active[type] ? active[type].call(this) : '';
+    });
+
+
+
+  
+  exports('goodlist', function(){
+    //alert('Hello World!');
+    return {aoe:'20',boe:35};
+    //设置该模块返回值
+  });
+});
--- src/lay/html/modules/index.js
+++ src/lay/html/modules/index.js
@@ -2,7 +2,7 @@
   项目JS主入口
   以依赖layui的layer和form模块为例
 **/    
-layui.define(['layer', 'form','element','table'], function(exports){
+layui.define(['layer', 'form','element'], function(exports){
   var layer = layui.layer
   ,form = layui.form,dome=layui.dome,element = layui.element,table=layui.table;
 
@@ -58,88 +58,19 @@ layui.define(['layer', 'form','element','table'], function(exports){
   //   ]]
   // });
 
-  layui.use('table', function(){
-    var table = layui.table;
-    
-    table.render({
-      elem: '#test'
-      ,url:'/goods'
-      ,toolbar: `<div class="layui-btn-container">
-      <button class="layui-btn layui-btn-sm" lay-event="getCheckData">获取选中行数据</button>
-      <button class="layui-btn layui-btn-sm" lay-event="getCheckLength">获取选中数目</button>
-      <button class="layui-btn layui-btn-sm" lay-event="isAll">验证是否全选</button>
-    </div>`
-      ,title: '商品列表'
-      ,cols: [[
-        {type: 'checkbox', fixed: 'left'}
-        ,{field:'SID', title:'ID', width:80, fixed: 'left', unresize: true, sort: true}
-        ,{field:'SNAME', title:'商品名称', width:120, edit: 'text'}
-        
-        ,{field:'STAG', title:'分类', width:80, edit: 'text', sort: true}
-        ,{field:'PRICE', title:'价格（原价）', width:120}
-        ,{field:'PRICE', title:'价格（现价）', width:120}
-        ,{field:'REPE', title:'库存', width:80, sort: true}
-        ,{field:'DIAN', title:'状态', width:120}
-        ,{field:'joinTime', title:'加入时间', width:150}
-        ,{fixed: 'right', title:'操作', toolbar: '#barDemo', width:200}
-      ]]
-      ,page: true
-      ,limit: 10
-    });
-    
-    //监听表格复选框选择
-    table.on('checkbox(demo)', function(obj){
-      console.log(obj)
-    });
-    //监听工具条
-    table.on('tool(barDemo)', function(obj){
-      var data = obj.data;
-      if(obj.event === 'detail'){
-        layer.msg('ID：'+ data.id + ' 的查看操作');
-      } else if(obj.event === 'del'){
-        layer.confirm('真的删除行么', function(index){
-          obj.del();
-          layer.close(index);
-        });
-      } else if(obj.event === 'edit'){
-        layer.alert('编辑行：<br>'+ JSON.stringify(data))
-      }
-    });
-    
-    var $ = layui.$, active = {
-      getCheckData: function(){ //获取选中数据
-        var checkStatus = table.checkStatus('idTest')
-        ,data = checkStatus.data;
-        layer.alert(JSON.stringify(data));
-      }
-      ,getCheckLength: function(){ //获取选中数目
-        var checkStatus = table.checkStatus('idTest')
-        ,data = checkStatus.data;
-        layer.msg('选中了：'+ data.length + ' 个');
-      }
-      ,isAll: function(){ //验证是否全选
-        var checkStatus = table.checkStatus('idTest');
-        layer.msg(checkStatus.isAll ? '全选': '未全选')
-      }
-    };
-    
-    $('.demoTable .layui-btn').on('click', function(){
-      var type = $(this).data('type');
-      active[type] ? active[type].call(this) : '';
-    });
-  
-    
-    
-  });
-
 
   exports('index', {}); //注意，这里是模块输出的核心，模块名必须和use时的模块名一致
 });
 
-layui.use(['demo'],function(demo){
-    console.log(demo());
-    xxx=demo();
-
+layui.use(['demo','goodlist'],function(demo,goodlist){
+    console.log(demo(),goodlist());
+    xxx.demo=demo;
+    xxx.goodlist=goodlist;
+    //得到引入模块的返回值,按照引入顺序
+    console.log(xxx.demo(),xxx.goodlist());
+         
 
 })
-var xxx='';
+
+//index页面公共对象
+var xxx={};
