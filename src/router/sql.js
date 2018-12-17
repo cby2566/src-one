71bd7530d4dafbf381b91e3e2eb3d7e78138fd68
peco <1535895782@qq.com>
2018-12-17 11:26:34 +0800

    11:26

--- src/router/sql.js
+++ src/router/sql.js
@@ -3,7 +3,7 @@ const mysql=require('mysql');
 var pool  = mysql.createPool({
     host     : 'localhost',
     user     : 'root',
-    password : '',
+    password : 'root',
     port: 3306,
     database: 'ugoshop'
 });
